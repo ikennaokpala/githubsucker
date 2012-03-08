94a422e3eb2882fd74e0295007a1e1c26c15045c
Ikenna Okpala <ikennaokpala@gmail.com>
2012-03-08 17:10:28 +0000

    Modify gitsucker and minitest_helper files
    * Project variable to project_name in gitsucker
    * Remove trace print for GithubSucker in minitest_helper

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -2,5 +2,3 @@ project_dir = File.expand_path(File.dirname(__FILE__) + "/..")
 require "minitest/autorun"
 require project_dir+"/lib/dashboard"
 require project_dir+"/lib/scrapper"
-
-puts GitHubSucker
