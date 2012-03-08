4b7d7518e3b1f8ff602d5eaece12cb33afb6f82e
Ikenna Okpala <ikennaokpala@gmail.com>
2012-03-08 18:15:22 +0000

    Add octocats/cockpit test and implementation

---
+++ lib/dashboard.rb
@@ -0,0 +1,43 @@
+require_relative "scrapper"
+require "octokit"
+
+module GitHubSucker 
+  module DashBoard
+    @@git = GitHubSucker::Scrapper.new
+
+    def self.cockpit(project_name)
+      printf "%12s%18s%18s%18s%20s\n", "User", "Own Projects", "Forked Projects", "Ruby Projects", "Javascript Projects"
+      octocats(project_name).each do |i|
+        printf "%13s%14d%14d%14d%14d\n", i[:name], i[:original], i[:forked], i[:ruby_projects], i[:js_projects]
+      end
+    end
+
+    def self.octocats(project_name)      
+      project = @@git.project_info_search(project_name)      
+      if project_name.nil?
+        puts red_alert "Project does not exist! Try again!"
+      else
+        rank_octocats project
+      end
+    end
+
+    private 
+        def self.rank_octocats(project_name)
+          users = []
+          Octokit.forks(project_name).each do |fk|
+            js_projects = 0
+            ruby_projects = 0
+            original = 0
+            forked = 0
+            repositories = Octokit.repositories(fk.owner.login)
+            repositories.each do |repo|
+              if repo.language && repo.language.downcase == 'ruby' ? ruby_projects = ruby_projects + 1 : repo.language && repo.language.downcase == 'javascript' ? js_projects = js_projects + 1 : nil
+                repo.fork ? forked = forked + 1 : original = original + 1
+              end
+              users << {:name => fk.owner.login, :num_repos => repositories.size, :js_projects => js_projects, :ruby_projects => ruby_projects, :forked => forked, :original => original}
+            end
+            users.sort_by!{|u| :original}.reverse!
+          end 
+        end
+  end
+end
--- test/unit/github_sucker_test.rb
+++ test/unit/github_sucker_test.rb
@@ -4,15 +4,37 @@ class GitHubSuckerTest < MiniTest::Unit::TestCase
   def setup
     @dashboard = GitHubSucker::DashBoard
     @scrapper = GitHubSucker::Scrapper.new
-    @url = "#{BASE_URL}/search?type=Repositories&q=nu&repo=&langOverride=&x=14&y=9&start_value=1"
   end
+
   def test_project_info_search
     search_result = @scrapper.project_info_search("nu").split("/")
     assert_equal "timburks", search_result.first
     assert_equal "nu", search_result[1]
   end
 
+  def test_invalid_project_info_search
+    search_result = @scrapper.project_info_search("timburks/nu")
+    assert_nil search_result
+  end
+
   def  test_page_doc
-     assert_kind_of Nokogiri::XML::Document, @scrapper.send(:page_doc, @url)
+    @url = "#{BASE_URL}/search?type=Repositories&q=nu&repo=&langOverride=&x=14&y=9&start_value=1"
+    assert_kind_of Nokogiri::XML::Document, @scrapper.send(:page_doc, @url)
+  end
+
+  def test_rank_octocats 
+    octocats = @dashboard.rank_octocats("timburks/nu")
+    assert_kind_of Enumerable, octocats
+  end
+
+  def test_octocats 
+    octocats = @dashboard.octocats("nu")
+    assert_kind_of Enumerable, octocats
+  end
+
+  def test_nil_project_name_octocats
+    assert_raises(Octokit::NotFound) do
+      @dashboard.octocats("timburks/nu")
+    end
   end
 end
