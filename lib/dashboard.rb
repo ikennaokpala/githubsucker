199a848a075201e26d50942babf6fff530171baa
Ikenna Okpala <ikennaokpala@gmail.com>
2012-03-09 02:04:40 +0000

    Rework

--- lib/dashboard.rb
+++ lib/dashboard.rb
@@ -9,7 +9,7 @@ module GitHubSucker
 
       printf "%12s%18s%18s%18s%20s\n", "User", "Own Projects", "Forked Projects", "Ruby Projects", "Javascript Projects"
       octocats(project_name).each do |i|
-        printf "%13s%14d%14d%14d%14d\n", i[:name], i[:original], i[:forked], i[:ruby_projects], i[:js_projects]
+        printf "%13s%14s%14s%14s%14s\n", i["name"], i["original"], i["forked"], i["ruby_projects"], i["js_projects"]
       end
     end
 
@@ -24,25 +24,19 @@ module GitHubSucker
 
     private 
     def self.rank_octocats(project_name)
-      users = []
+      users = Hash.new
       Octokit.forks(project_name).each do |fk|
-        js_projects = 0
-        ruby_projects = 0
-        original = 0
-        forked = 0
         repositories = Octokit.repositories(fk.owner.login)
-        repositories.each do |repo|
-
-          repo.language && repo.language.downcase == 'ruby' ? ruby_projects += 1 : repo.language && repo.language.downcase == 'javascript' ? js_projects += 1 : nil
-
-          repo.fork ? forked += 1 : original +=  1
-        end
-
-        users << {:name => fk.owner.login, :num_repos => repositories.size, :js_projects => js_projects, :ruby_projects => ruby_projects, :forked => forked, :original => original}
-
+        users["name"] = fk.owner.login
+        users["num_repos"] = repositories.size
+        users["js_projects"] = repositories.map{|r| r.language && r.language.downcase == "javascript" ? 1 : nil }.compact.size
+        users["ruby_projects"] = repositories.map{|r| r.language && r.language.downcase == "ruby"? 1 : nil}.compact.size
+        users["forked"] = repositories.map{|r| r.fork ? 1 : nil}.compact.size
+        users["original"] = repositories.map{|r| !r.fork ? 1 : nil}.compact.size
       end
-      users.sort_by!{|u| u["original"]}.reverse!
+      [users]#.sort_by!{|u| u["original"]}.reverse!
     end 
   end
 end
+
 
